9fdd34789ad6bfb22ea212113e01391dde34010d
isobelojalvo <ojalvo@wisc.edu>
2018-07-12 07:59:31 -0500

    clean up analyzer

--- test/test-Analyzer.py
+++ test/test-Analyzer.py
@@ -1,79 +1,110 @@
-# define basic process
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: step2 --python_filename=rerun_step2_L1_onMCL1_FEVTHLTDEBUG.py --no_exec -s L1 --datatier GEN-SIM-DIGI-RAW -n 1 --era Phase2_timing --eventcontent FEVTDEBUGHLT --filein file:/afs/cern.ch/user/r/rekovic/release/CMSSW_9_3_2/src/step2_DIGI_PU200_10ev.root --conditions 93X_upgrade2023_realistic_v2 --beamspot HLLHC14TeV --geometry Extended2023D17 --fileout file:step2_ZEE_PU200_1ev_rerun-L1-L1Ntuple.root --customise=L1Trigger/L1TNtuples/customiseL1Ntuple.L1NtupleEMU
 import FWCore.ParameterSet.Config as cms
-import os
-process = cms.Process("L1CaloClusters")
- 
-# import standard configurations
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('L1',eras.Phase2_trigger)
+
+# import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
-#process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
-#process.load('Configuration.Geometry.GeometryExtended2023D4_cff')
-process.load('Configuration.Geometry.GeometryExtended2023D13Reco_cff') ## this needs to match the geometry you are running on
-process.load('Configuration.Geometry.GeometryExtended2023D13_cff')     ## this needs to match the geometry you are running on
-
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D17Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.load('L1Trigger.TrackFindingTracklet.L1TrackletTracks_cff')
 
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(3000)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/MINIAODSIM/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/C033607A-8E2C-E811-B4EF-0CC47A78A478.root'),
+    secondaryFileNames = cms.untracked.vstring(
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/02306B8E-6D2C-E811-9625-0025905B858C.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/163A5C8E-6D2C-E811-B990-0025905B85B2.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/1A7E6D8F-6D2C-E811-8DFB-0025905B85BE.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/5864268A-6D2C-E811-90F5-0025905A60EE.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/627E087F-6D2C-E811-8F81-0CC47A4C8E34.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/64AE2B7F-6D2C-E811-800E-0CC47A4D76A0.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/820C0278-6D2C-E811-B95B-0CC47A4C8F0C.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/BE6A3C80-6D2C-E811-9143-0CC47A4D7668.root',
+        'root://cmsxrootd.fnal.gov///store/relval/CMSSW_9_3_7/RelValZTT_14TeV/GEN-SIM-DIGI-RAW/93X_upgrade2023_realistic_v5_2023D17noPU-v2/10000/DA7C408A-6D2C-E811-9BFF-0025905B858A.root'
+        ),
+    inputCommands = cms.untracked.vstring("keep *", 
+        "drop l1tHGCalTowerMapBXVector_hgcalTriggerPrimitiveDigiProducer_towerMap_HLT",
+        "drop l1tEMTFHit2016Extras_simEmtfDigis_CSC_HLT",
+        "drop l1tEMTFHit2016Extras_simEmtfDigis_RPC_HLT",
+        "drop l1tEMTFHit2016s_simEmtfDigis__HLT",
+        "drop l1tEMTFTrack2016Extras_simEmtfDigis__HLT",
+        "drop l1tEMTFTrack2016s_simEmtfDigis__HLT")
+     #skipEvents = cms.untracked.uint32(80)
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step2 nevts:1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RAW'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:test_reprocess.root'),
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '100X_upgrade2023_realistic_v1', '')
 
 
-# input
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1000))
-Source_Files = cms.untracked.vstring(
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SinglePion_FlatPt-8to100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/70000/00469E3E-7D26-E711-BBE0-5065F37D9132.root'
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SinglePion0_FlatPt-8to100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/70000/0024CEE8-5829-E711-A2CA-5065F382C231.root'
-    #'file:singleE-gen-sim-raw.root'
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleE_FlatPt-8to100/GEN-SIM-DIGI-RAW/PU200_90X_upgrade2023_realistic_v9-v1/120000/002A4121-132C-E711-87AD-008CFAFBF618.root'
-    #'file:/hdfs/store/mc/PhaseIISpring17D/GluGluHToZZTo4L_M125_14TeV_powheg2_JHUgenV702_pythia8/GEN-SIM-DIGI-RAW/PU200_100M_90X_upgrade2023_realistic_v9-v1/120000/8882318A-8E44-E711-AA96-0242AC130002.root'
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/TauThreeProngsEnriched/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/042B46D1-8E55-E711-A6C7-0CC47AA53D5A.root'
-    '/store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/PU140_MB100M_90X_upgrade2023_realistic_v9-v1/120000/0052D3DF-4C56-E711-84A9-0025907DE22C.root'
+process.load('SimCalorimetry.HcalTrigPrimProducers.hcaltpdigi_cff')
+process.load('CalibCalorimetry.CaloTPG.CaloTPGTranscoder_cfi')
 
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/02E3D76F-8A55-E711-B9F0-0025901FB188.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/044D566A-C157-E711-BEDE-0CC47A0AD6AA.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/069E68E8-C457-E711-8E15-0CC47A0AD742.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/08AF49F4-8A55-E711-A870-0CC47A0AD74E.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/0AFC8973-C557-E711-8578-0025907859B8.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/0E4E7912-7857-E711-A03C-0CC47AA53DBE.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/0E4FFCB6-8B57-E711-A904-0CC47A57CB8E.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/0E7A1838-5557-E711-8B7B-0CC47A57CB8E.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/0ECD0F32-B157-E711-B00C-0025901FB188.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/109938E4-7B57-E711-92C4-0CC47A57CB8E.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/120AE875-B357-E711-AAC3-0025907D24F0.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/129004EE-C557-E711-B4A3-00259075D62E.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/18113155-C457-E711-812B-0CC47A57CD6A.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/188CA032-C057-E711-913E-0CC47A57CD6A.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/18C21A5B-C457-E711-927F-002590FD5A4C.root',
-    #'root://cms-xrd-global.cern.ch//store/mc/PhaseIISpring17D/SingleTauOneProngFlatPt10To100/GEN-SIM-DIGI-RAW/NoPU_90X_upgrade2023_realistic_v9-v1/120000/1AB4EFC0-B357-E711-8479-0CC47A57CB8E.root'
-    )
-process.source = cms.Source("PoolSource", fileNames = Source_Files)
+process.load('L1Trigger.L1THGCal.hgcalTriggerPrimitives_cff')
+process.hgcl1tpg_step = cms.Path(process.hgcalTriggerPrimitives)
 
-############################################################
-# L1 tracking
-############################################################
+process.load('SimCalorimetry.EcalEBTrigPrimProducers.ecalEBTriggerPrimitiveDigis_cff')
+process.EcalEBtp_step = cms.Path(process.simEcalEBTriggerPrimitiveDigis)
 
-# remake stubs 
-# ===> IMPORTANT !!! stub window tuning as is by default in CMSSW is incorrect !!! <===
-process.load('L1Trigger.TrackTrigger.TrackTrigger_cff')
-from L1Trigger.TrackTrigger.TTStubAlgorithmRegister_cfi import *
-process.TTClusterStub = cms.Path(process.TrackTriggerClustersStubs)
+process.L1TrackTrigger_step = cms.Path(process.L1TrackletTracksWithAssociators)
 
-process.load("L1Trigger.TrackFindingTracklet.L1TrackletTracks_cff")
+process.VertexProducer.l1TracksInputTag = cms.InputTag("TTTracksFromTracklet", "Level1TTTracks")
 
-# run only the tracking (no MC truth associators)
-process.TTTracks = cms.Path(process.L1TrackletTracks)
+# Path and EndPath definitions
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
 
-# run the tracking AND MC truth associators)
-process.TTTracksWithTruth = cms.Path(process.L1TrackletTracksWithAssociators)
 
 ############################################################
 # L1 clusters
 ############################################################
 
 process.load("L1Trigger.phase2Demonstrator.L1CaloClusterProducer_cff")
-process.L1CaloClusterProducer.debug = cms.untracked.bool(False)
+process.L1CaloClusterProducer.debug = cms.untracked.bool(True)
 process.L1Clusters = cms.Path(process.L1CaloClusterProducer)
 
 ############################################################
@@ -81,14 +112,14 @@ process.L1Clusters = cms.Path(process.L1CaloClusterProducer)
 ############################################################
 
 process.load("L1Trigger.phase2Demonstrator.L1PFProducer_cff")
-process.L1PFProducer.debug = cms.untracked.bool(False)
+process.L1PFProducer.debug = cms.untracked.bool(True)
 process.L1PFObjects = cms.Path(process.L1PFProducer)
 
 ############################################################
 # L1 Tau object
 ############################################################
 process.load("L1Trigger.phase2Demonstrator.L1PFTauProducer_cff")
-process.L1PFTauProducer.debug = cms.untracked.bool(False)
+process.L1PFTauProducer.debug = cms.untracked.bool(True)
 process.L1PFTaus = cms.Path(process.L1PFTauProducer)
 
 
@@ -96,22 +127,19 @@ process.L1PFTaus = cms.Path(process.L1PFTauProducer)
 process.load("L1Trigger.phase2L1TauAnalyzer.phase2L1TauAnalyzer_cfi")
 process.analyzer = cms.Path(process.L1TauAnalyzer)
 
-# output module
-#process.out = cms.OutputModule( "PoolOutputModule",
-#                                fileName = cms.untracked.string("CaloClusters.root"),
-#                                fastCloning = cms.untracked.bool( False ),
-#                                outputCommands = cms.untracked.vstring(#'drop *',
-#                                                                       'keep *_*_L1Phase2CaloClusters_*', 
-#                                                                       )
-#)
-#process.FEVToutput_step = cms.EndPath(process.out)
 
 process.TFileService = cms.Service("TFileService", 
-   fileName = cms.string("analyzer-1Prong.root"), 
+   fileName = cms.string("analyzer.root"), 
    closeFileFast = cms.untracked.bool(True)
 )
 
+# Schedule definition
+process.schedule = cms.Schedule(process.EcalEBtp_step,process.L1Clusters,process.L1TrackTrigger_step,process.L1simulation_step,process.L1PFObjects,process.L1PFTaus,process.analyzer,process.endjob_step) 
 
-process.schedule = cms.Schedule(process.L1Clusters,process.TTClusterStub,process.TTTracksWithTruth,process.L1PFObjects,process.L1PFTaus,process.analyzer)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
 
-
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
