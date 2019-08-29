683426d6e8a6a7807d8862124d8b75f5af800d91
Stephanie Kwan <skwan@princeton.edu>
2019-08-29 15:25:19 -0500

    Files used to create 2019_Aug28-DYToLL_NoPU_withL1Tracks-DYToLL-M50-NoPU.root

--- test/two-file-batch-example/createAnalyzers.sh
+++ test/two-file-batch-example/createAnalyzers.sh
@@ -1,8 +1,7 @@
-jobName="2019_Aug16-NeutrinoGun_E_10GeV_PU200"
-# 
-
+jobName="2019_Aug28-DYToLL_NoPU_withL1Tracks"
+#
 j=0
-for i in {0..1}
+for i in {0..35}
 do
     cat test-Analyzer.py > SUB-Analyzer-${i}.py
     #echo "process.source.skipEvents = cms.untracked.uint32(${j})" >> SUB-Analyzer-${i}.py
@@ -10,10 +9,9 @@ do
     cat submit.py >> SUB-Analyzer-${i}.py
     cat submit-$i.py >> SUB-Analyzer-${i}.py
 
-    mkdir -p /nfs_scratch/skkwan/${jobName}/SUB-NeutrinoGun_E_10GeV_PU200-14TeV-SUBPhase-$i/dags/daginputs
+    mkdir -p /nfs_scratch/skkwan/${jobName}/SUB-DYToLL-M-50-NoPU-14TeV-SUBPhase-$i/dags/daginputs
 
-    # removing  --vsize-limit=7000 from below command
-    farmoutAnalysisJobs --assume-input-files-exist  --input-file-list=inputFileList-dummy.txt --output-dir=/hdfs/store/user/skkwan/${jobName} --submit-dir=/nfs_scratch/skkwan/${jobName}/SUB-NeutrinoGun_E_10GeV_PU200-SUBPhase-$i/submit --output-dag-file=/nfs_scratch/skkwan/${jobName}/SUB-NeutrinoGun_E_10GeV_PU200-SUBPhase-$i/dags/dag  ${jobName}-NeutrinoGun_E_10GeV-PU200  $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/phase2L1TauAnalyzer/test/two-file-batch-example/SUB-Analyzer-$i.py     &
+    farmoutAnalysisJobs --vsize-limit=7000 --assume-input-files-exist  --input-file-list=inputFileList-dummy.txt --output-dir=/hdfs/store/user/skkwan/${jobName} --submit-dir=/nfs_scratch/skkwan/${jobName}/SUB-DYToLL-M50-NoPU-SUBPhase-$i/submit --output-dag-file=/nfs_scratch/skkwan/${jobName}/SUB-DYToLL-M50-NoPU-SUBPhase-$i/dags/dag  ${jobName}-DYToLL-M50-NoPU  $CMSSW_BASE  $CMSSW_BASE/src/L1Trigger/phase2L1TauAnalyzer/test/two-file-batch-example/SUB-Analyzer-$i.py     &
     
     if [ "$i" -eq "15" ]; then
 	wait;
