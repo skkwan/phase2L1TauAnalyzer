16f02d84b67e0a1e98e0a1427d94c8ec0489c215
Stephanie Kwan <skwan@princeton.edu>
2019-09-17 08:30:41 -0500

    2019_Sep16-ggHtautau_noPU-MINI File paths

--- test/aodsim-two-file-batch/createAnalyzers.sh
+++ test/aodsim-two-file-batch/createAnalyzers.sh
@@ -1,7 +1,7 @@
-jobName="2019_Aug29-ggHtautau_noPU-try2"
+jobName="2019_Sep16-ggHtautau_noPU-MINI"
 #
 j=0
-for i in {0..11}
+for i in {0..1}
 do
     cat test-Analyzer.py > SUB-Analyzer-${i}.py
     #echo "process.source.skipEvents = cms.untracked.uint32(${j})" >> SUB-Analyzer-${i}.py
